70b9b3a4c2e31a0c0af98c8c1a06639dbab63280
mi2507 <mreginadasilva@yahoo.com.br>
2021-01-30 19:18:47 -0300

    aula4 completa com sucesso

---
+++ pages/quiz/[id].js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function QuizDaGaleraPage() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div>
+      Desafio da próxima aula junto com as animações
+    </div>
+  );
+}
